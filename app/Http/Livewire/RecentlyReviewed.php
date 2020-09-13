ef8da57264373bcc0d6bb60a056ea9e6225c2a73
absmugz <absolomm@roam.africa>
2020-09-13 11:17:38 +0200

    refactoring views to make them slimmer

--- app/Http/Livewire/RecentlyReviewed.php
+++ app/Http/Livewire/RecentlyReviewed.php
@@ -2,34 +2,50 @@
 
 namespace App\Http\Livewire;
 
+use Carbon\Carbon;
 use Livewire\Component;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Carbon;
 
 class RecentlyReviewed extends Component
 {
-    public array $recentlyReviewed = [];
-    public function loadRecentlyReviewed(){
+    public $recentlyReviewed = [];
 
+    public function loadRecentlyReviewed()
+    {
         $before = Carbon::now()->subMonths(2)->timestamp;
-        $current  = Carbon::now()->timestamp;
+        $current = Carbon::now()->timestamp;
 
-        $this->recentlyReviewed = Http::withHeaders(config('services.igdb'))->withOptions([
-            'body' => "
-            fields name, cover.url, first_release_date, popularity, platforms.abbreviation, rating, rating_count, summary, slug;
-            where platforms = (48,49,130,6)
-            & (first_release_date > {$before}
-            & first_release_date < {$current}
-            & rating_count > 5);
-            sort popularity desc;
-            limit 3;
-            "
-        ])->get('https://api-v3.igdb.com/games/')
-        ->json();
+         $recentlyReviewedUnformatted = Http::withHeaders(config('services.igdb'))
+            ->withOptions([
+                'body' => "
+                    fields name, cover.url, first_release_date, popularity, platforms.abbreviation, rating, rating_count, summary, slug;
+                    where platforms = (48,49,130,6)
+                    & (first_release_date >= {$before}
+                    & first_release_date < {$current}
+                    & rating_count > 5);
+                    sort popularity desc;
+                    limit 3;
+                "
+            ])->get('https://api-v3.igdb.com/games')
+            ->json();
 
+        $this->recentlyReviewed = $this->formatForView($recentlyReviewedUnformatted);
     }
+
     public function render()
     {
         return view('livewire.recently-reviewed');
+    }
+
+    private function formatForView($games)
+    {
+        return collect($games)->map(function ($game) {
+            return collect($game)->merge([
+                'coverImageUrl' => Str::replaceFirst('thumb','cover_big', $game['cover']['url']),
+                'rating' => isset($game['rating']) ? round($game['rating']).'%' : null,
+                'platforms' => collect($game['platforms'])->pluck('abbreviation')->implode(', '),
+            ]);
+        })->toArray();
     }
 }
